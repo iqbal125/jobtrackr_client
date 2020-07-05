4ee46e0f4e8dcfd7ee9b6104b36bc7043a2d8c6d
iqbal125 <iqbal125@yahoo.com>
2020-07-04 17:33:33 -0700

    minor refactor

--- src/components/GetJobs/index.js
+++ src/components/GetJobs/index.js
@@ -4,41 +4,22 @@ import axios from 'axios';
 import styles from './getjobs.module.css';
 import MaterialTable from 'material-table';
 
-// Find out which icons actually need.
-import AddBox from '@material-ui/icons/AddBox';
 import ArrowDownward from '@material-ui/icons/ArrowDownward';
-import Check from '@material-ui/icons/Check';
 import ChevronLeft from '@material-ui/icons/ChevronLeft';
 import ChevronRight from '@material-ui/icons/ChevronRight';
 import Clear from '@material-ui/icons/Clear';
-import DeleteOutline from '@material-ui/icons/DeleteOutline';
-import Edit from '@material-ui/icons/Edit';
-import FilterList from '@material-ui/icons/FilterList';
 import FirstPage from '@material-ui/icons/FirstPage';
 import LastPage from '@material-ui/icons/LastPage';
-import Remove from '@material-ui/icons/Remove';
-import SaveAlt from '@material-ui/icons/SaveAlt';
 import Search from '@material-ui/icons/Search';
-import ViewColumn from '@material-ui/icons/ViewColumn';
 
 const tableIcons = {
-  Add: forwardRef((props, ref) => <AddBox {...props} ref={ref} />),
-  Check: forwardRef((props, ref) => <Check {...props} ref={ref} />),
-  Clear: forwardRef((props, ref) => <Clear {...props} ref={ref} />),
-  Delete: forwardRef((props, ref) => <DeleteOutline {...props} ref={ref} />),
-  DetailPanel: forwardRef((props, ref) => <ChevronRight {...props} ref={ref} />),
-  Edit: forwardRef((props, ref) => <Edit {...props} ref={ref} />),
-  Export: forwardRef((props, ref) => <SaveAlt {...props} ref={ref} />),
-  Filter: forwardRef((props, ref) => <FilterList {...props} ref={ref} />),
   FirstPage: forwardRef((props, ref) => <FirstPage {...props} ref={ref} />),
   LastPage: forwardRef((props, ref) => <LastPage {...props} ref={ref} />),
   NextPage: forwardRef((props, ref) => <ChevronRight {...props} ref={ref} />),
   PreviousPage: forwardRef((props, ref) => <ChevronLeft {...props} ref={ref} />),
   ResetSearch: forwardRef((props, ref) => <Clear {...props} ref={ref} />),
   Search: forwardRef((props, ref) => <Search {...props} ref={ref} />),
-  SortArrow: forwardRef((props, ref) => <ArrowDownward {...props} ref={ref} />),
-  ThirdStateCheck: forwardRef((props, ref) => <Remove {...props} ref={ref} />),
-  ViewColumn: forwardRef((props, ref) => <ViewColumn {...props} ref={ref} />)
+  SortArrow: forwardRef((props, ref) => <ArrowDownward {...props} ref={ref} />)
 };
 
 const user = {
@@ -73,40 +54,20 @@ const GetJobs = () => {
 
   const jobsPlaceHolder = [{ position: '' }];
 
-  // change  to await async
   const deleteJob = () => {
-    let handleRes = res => {
-      console.log(res);
-    };
-
-    let handleErr = err => {
-      console.log(err);
-    };
-
     let data = {
       user_id: user.id
     };
 
     //axios
     //  .delete(`${process.env.GATSBY_SERVER_URL}/api/users/deleteJob/${job.id}`, { data })
-    //  .then(res => handleRes(res))
-    //  .catch(err => handleErr(err));
+    //  .then(res => console.log(res))
+    //  .catch(err => console.log(err));
   };
 
-  const fetchJobs = () => {
-    let handleRes = res => {
-      console.log(res);
-      setJobs(res.data);
-    };
-
-    let handleErr = err => {
-      console.log(err);
-    };
-
-    axios
-      .get(`${process.env.GATSBY_SERVER_URL}/api/users/getJob/${user.id}`)
-      .then(res => handleRes(res))
-      .catch(err => handleErr(err));
+  const fetchJobs = async () => {
+    let results = await axios.get(`${process.env.GATSBY_SERVER_URL}/api/users/getJob/${user.id}`);
+    setJobs(results.data);
   };
 
   useEffect(() => {
@@ -116,13 +77,15 @@ const GetJobs = () => {
   return (
     <div>
       <div>
-        <div onClick={() => navigate('/app/addjob')}>Add Job</div>
-        <MaterialTable
-          icons={tableIcons}
-          columns={columns}
-          data={jobs ? jobs : jobsPlaceHolder}
-          title="Jobs"
-        />
+        <button onClick={() => navigate('/app/addjob')}>Add Job</button>
+        <div className={styles.table_container}>
+          <MaterialTable
+            icons={tableIcons}
+            columns={columns}
+            data={jobs ? jobs : jobsPlaceHolder}
+            title="Jobs"
+          />
+        </div>
       </div>
     </div>
   );
