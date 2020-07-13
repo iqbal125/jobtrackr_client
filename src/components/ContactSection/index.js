2febb3a7a666ccb76f9b6e8ed7bf28836cc22d12
iqbal125 <iqbal125@yahoo.com>
2020-07-12 18:09:12 -0700

    finish contact form

--- src/components/ContactSection/index.js
+++ src/components/ContactSection/index.js
@@ -1,17 +1,12 @@
 import React from 'react';
 import ContactForm from '../Form';
 import styles from './contactsection.module.css';
-import image2 from '../../../static/uploads/wave2.svg';
 
 const ContactSection = () => {
   return (
-    <div className={styles.top_div}>
-      <div className={styles.page_column}>
-        <h2 className={styles.cta}>Get in Touch with Any Questions or Concerns</h2>
-        <ContactForm />
-        <img src={image2} alt="" />
-        <div className={styles.background}> </div>
-      </div>
+    <div className={styles.page_column}>
+      <h2 className={styles.cta}>Get in Touch with Any Questions or Concerns</h2>
+      <ContactForm />
     </div>
   );
 };
--- src/components/Form/index.js
+++ src/components/Form/index.js
@@ -1,56 +1,92 @@
-import React, { useState } from 'react';
-import styles from './contactform.module.css';
-import { FcInvite } from 'react-icons/fc';
+import React, { useState, useContext } from 'react';
 import { navigate } from 'gatsby';
+import { Formik } from 'formik';
+import styles from './contactform.module.css';
+import * as Yup from 'yup';
+
+const ValidSchema = Yup.object().shape({
+  email: Yup.string()
+    .email('Invalid email')
+    .required('Required'),
+  subject: Yup.string()
+    .min(3, 'Subject must be at least 3 Characters')
+    .max(50, 'Subject Too Long')
+    .required('Subject Required')
+});
 
 const ContactForm = () => {
-  const [isLoading, setLoading] = useState(false);
+  const [loading, setLoading] = useState(false);
 
   const handleSubmit = event => {
     event.preventDefault();
+    console.log('ffff');
     setLoading(true);
     setTimeout(() => navigate('/formsubmit'), 800);
   };
 
   return (
-    <div className={styles.form_wrap}>
-      <div className={styles.icon}>
-        <FcInvite />
-      </div>
-      {isLoading && (
+    <div>
+      {loading && (
         <>
           <div className={styles.loader}></div>
           <div className={styles.loading_background}></div>
         </>
       )}
-      <form
-        className={styles.main_form}
-        onSubmit={handleSubmit}
-        name="Contact Form"
-        method="POST"
-        data-netlify="true"
-      >
-        <input type="hidden" name="form-name" value="Contact Form" />
-        <div className={styles.form_row}>
-          <label>Your Name:</label>
-          <input type="name" name="name" />
-        </div>
-        <div className={styles.form_row}>
-          <label>Your Email:</label>
-          <input type="email" name="email" />
-        </div>
-        <div className={styles.form_row}>
-          <label>Subject:</label>
-          <input name="subject" />
-        </div>
-        <div className={styles.form_row}>
-          <label>Message:</label>
-          <textarea name="message" />
-        </div>
-        <button className={styles.submit_button} type="submit">
-          Send
-        </button>
-      </form>
+      <div className={styles.form_wrap}>
+        <Formik
+          validationSchema={ValidSchema}
+          initialValues={{ email: '', subject: '', description: '' }}
+          onSubmit={handleSubmit}
+        >
+          {({ values, errors, touched, handleChange, handleBlur, handleSubmit, isSubmitting }) => (
+            <form className={styles.form} onSubmit={handleSubmit}>
+              <label className={styles.input_label} htmlFor="email">
+                Email
+              </label>
+              <input
+                className={styles.form_input}
+                type="email"
+                name="email"
+                id="email"
+                onChange={handleChange}
+                onBlur={handleBlur}
+                value={values.email}
+              />
+              {errors.email && touched.email && (
+                <span className={styles.error_text}>{errors.email}</span>
+              )}
+              <label className={styles.input_label} htmlFor="subject">
+                Subject
+              </label>
+              <input
+                className={styles.form_input}
+                name="subject"
+                id="subject"
+                onChange={handleChange}
+                onBlur={handleBlur}
+                value={values.subject}
+              />
+              {errors.subject && touched.subject && (
+                <span className={styles.error_text}>{errors.subject}</span>
+              )}
+              <label className={styles.input_label} htmlFor="description">
+                Description
+              </label>
+              <textarea
+                className={styles.form_textarea}
+                name="description"
+                id="description"
+                onChange={handleChange}
+                onBlur={handleBlur}
+                value={values.description}
+              />
+              <button type="submit" className={styles.form_button}>
+                Submit
+              </button>
+            </form>
+          )}
+        </Formik>
+      </div>
     </div>
   );
 };
