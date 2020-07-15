88aae1757fea4f58abe6bf66a3c87f678d8d7d1f
iqbal125 <iqbal125@yahoo.com>
2020-07-14 17:30:00 -0700

    added about page styling

--- src/components/AboutSection/index.js
+++ src/components/AboutSection/index.js
@@ -1,14 +1,20 @@
 import React from 'react';
 import styles from './about.module.css';
 import image1 from '../../../static/uploads/coding_.svg';
+import { navigate } from 'gatsby';
 
 const AboutSection = () => {
   return (
-    <div className={styles.top_row}>
-      <img className={styles.hero_illustration} src={image1} alt="" />
-      <div className={styles.about_text}>
-        <h3>Avoid and Headaches and Choas Usually Associated with Job Hunting</h3>
-        <p>JobTrackr makes the entire job hunting process easy and streamlined</p>
+    <div>
+      <div className={styles.feature1}>
+        <div className={styles.feature1_text}>
+          <h3>Avoid the Headaches and Choas Usually Associated with Job Hunting</h3>
+          <p>JobTrackr makes the entire job hunting process easy and streamlined</p>
+          <button className={styles.signup_button} onClick={() => navigate('/app/login')}>
+            Sign Up Now
+          </button>
+        </div>
+        <img className={styles.feature1_img} src={image1} alt="" />
       </div>
     </div>
   );
