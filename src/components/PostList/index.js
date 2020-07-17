fcf82f91bfb070632207a5c21230b42e66b044b2
iqbal125 <iqbal125@yahoo.com>
2020-07-16 17:54:16 -0700

    fix image prop

--- src/components/PostList/index.js
+++ src/components/PostList/index.js
@@ -9,7 +9,7 @@ const PostList = ({ post, excerpt }) => {
   return (
     <Link className={styles.link} to={`${post.path}`}>
       <div className={styles.post_layout}>
-        <Img className={styles.feature_image} fixed={featuredImg} />
+        <Img className={styles.feature_image} fluid={featuredImg} />
         <div className={styles.post_text_container}>
           <h3>{post.title}</h3>
           <small>
