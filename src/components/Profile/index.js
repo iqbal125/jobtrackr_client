59b71a16ff09bb56cb137afa826ba2ae3a978d7d
iqbal125 <iqbal125@yahoo.com>
2020-08-09 18:55:26 -0700

    add client only comment

--- src/components/Profile/index.js
+++ src/components/Profile/index.js
@@ -1,5 +1,8 @@
 import React from 'react';
 import Loadable from '@loadable/component';
+
+//material-table is a client side only package and
+//will cause the build to fail, using loadable solves this.
 const LoadableTable = Loadable(() => import('../Table'));
 
 const Profile = () => {
