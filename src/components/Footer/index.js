cd48e30b98fb19da65db93d1d9f3a22ea313901c
iqbal125 <iqbal125@yahoo.com>
2020-09-30 17:36:59 -0700

    fix footer

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -10,15 +10,6 @@ const Footer = () => {
         <img className={styles.footer_wave} src={image7} alt="" />
         <div className={styles.footer_main}>
           <div className={styles.left_footer}>
-            <small className={styles.footer_link}>1000 Example Rd, San Francisco, CA 94066 </small>
-            <small className={styles.footer_link}>1-111-1111 </small>
-            <small className={styles.footer_link}>Example@example.com</small>
-            <div className={styles.legal_section}>
-              <small className={styles.footer_legal}>Terms of Condition </small>
-              <small className={styles.footer_legal}>Privacy Policy</small>
-            </div>
-          </div>
-          <div className={styles.right_footer}>
             <Link to="/contact">
               <div className={styles.footer_link}>Contact</div>
             </Link>
@@ -29,6 +20,7 @@ const Footer = () => {
               <div className={styles.footer_link}>About</div>
             </Link>
           </div>
+          <div className={styles.right_footer}></div>
         </div>
         <div className={styles.footer_bottom}>
           <div> Copyright &copy; 2020 Example Inc</div>
